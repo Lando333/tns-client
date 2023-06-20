9c9d5e76d34bd05523b4459838fb451d9672b5f5
Ryan Rojas <landoslair@gmail.com>
2023-06-20 17:18:51 -0400

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import AppRouter from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>
+  <AppRouter />
 );
