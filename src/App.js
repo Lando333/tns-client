263dc03b7cb06861a5813a2a32a80bcbbba1804f
Ryan Rojas <landoslair@gmail.com>
2023-06-20 21:49:24 -0400

    added props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
 
-const App = ({ children }) => {
+const App = ({ baseUrl }) => {
+    
     return (
         <div>
-            <NavBar />
-            <div>{children}</div>
+            <NavBar baseUrl={baseUrl} />
+            <Outlet />
         </div>
     );
 };
