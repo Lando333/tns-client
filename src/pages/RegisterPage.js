d3c2fb9f2a2feaba1fd7070ec5365d025cb4ed2b
Ryan Rojas <landoslair@gmail.com>
2023-06-21 14:45:08 -0400

    added full address form

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -7,13 +7,27 @@ const RegisterPage = ({baseUrl}) => {
     const [first_name, setFirstName] = useState("")
     const [last_name, setLastName] = useState("")
 
+    const [address_line1, setAddressLineOne] = useState("")
+    const [address_line2, setAddressLineTwo] = useState("")
+    const [city, setCity] = useState("")
+    const [state, setState] = useState("")
+    const [zip_code, setZipCode] = useState("")
+    const [country, setCountry] = useState("")
+
+
     const registerUser = async () => {
         try {
             const resp = await httpClient.post(baseUrl + "/register", {
                 email,
                 password,
                 first_name,
-                last_name
+                last_name,
+                address_line1,
+                address_line2,
+                city,
+                state,
+                zip_code,
+                country
             });
 
             window.location.href = "/";
@@ -34,39 +48,82 @@ const RegisterPage = ({baseUrl}) => {
                         type="text"
                         value={first_name}
                         onChange={(e) => setFirstName(e.target.value)}
-                        id=""
                     />
                 </div>
-
                 <div>
                     <label>Last Name: </label>
                     <input
                         type="text"
                         value={last_name}
                         onChange={(e) => setLastName(e.target.value)}
-                        id=""
                     />
                 </div>
-
                 <div>
                     <label>Email: </label>
                     <input
                         type="text"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        id=""
                     />
                 </div>
-
                 <div>
                     <label>Password: </label>
                     <input
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        id=""
                     />
                 </div>
+
+                <div>
+                    <label>Address Line 1: </label>
+                    <input
+                        type="text"
+                        value={address_line1}
+                        onChange={(e) => setAddressLineOne(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Address Line 2: </label>
+                    <input
+                        type="text"
+                        value={address_line2}
+                        onChange={(e) => setAddressLineTwo(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>City: </label>
+                    <input
+                        type="text"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>State: </label>
+                    <input
+                        type="text"
+                        value={state}
+                        onChange={(e) => setState(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Zip Code: </label>
+                    <input
+                        type="text"
+                        value={zip_code}
+                        onChange={(e) => setZipCode(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Country: </label>
+                    <input
+                        type="text"
+                        value={country}
+                        onChange={(e) => setCountry(e.target.value)}
+                    />
+                </div>
+
                 <button type="button" onClick={() => registerUser()}>
                     Submit
                 </button>
