0ec38ac8a1b61eada703337678ef7cab16816ba1
Ryan Rojas <landoslair@gmail.com>
2023-06-22 16:25:05 -0400

    functional scheduler

--- src/pages/SchedulePage.js
+++ src/pages/SchedulePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import { setHours, setMinutes } from "date-fns";
+import { addMinutes } from "date-fns";
 import format from "date-fns/format";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
@@ -11,9 +11,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { UserContext } from "../UserContext";
 
-const locales = {
-    "en-US": require("date-fns/locale/en-US"),
-};
+const locales = {"en-US": require("date-fns/locale/en-US"),};
 const localizer = dateFnsLocalizer({
     format,
     parse,
@@ -41,46 +39,59 @@ const events = [
 ];
 
 const SchedulePage = ({ baseUrl }) => {
-    const [newEvent, setNewEvent] = useState({
-        title: "",
-        start: new Date(),
-        startTime: "",
-        end: new Date(),
-        therapist: "",
-        service: "",
-    });
-    const [allEvents, setAllEvents] = useState([]);
-    const [therapists, setTherapists] = useState([]);
-    const [selectedTherapist, setSelectedTherapist] = useState("");
-    const [services, setServices] = useState([]);
-    const [selectedService, setSelectedService] = useState("");
-    const [duration, setDuration] = useState(60);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
+    // Values for formatting Scheduler and Time Picker
     const minTime = new Date();
     minTime.setHours(12, 0, 0);
     const maxTime = new Date();
     maxTime.setHours(21, 0, 0);
+    // For calendar
     const timeFormat = (date, culture, localizer) =>
         format(date, "h a", { locale: localizer.locale });
+    // For time picker
+    // const formatTime = (date) => {
+    //     if (!date) {
+    //         return ""; // Handle empty case
+    //     }
+    //     const hours = date.getHours();
+    //     const minutes = date.getMinutes();
+    //     const period = hours >= 12 ? "PM" : "AM";
+    //     const formattedHours = hours % 12 || 12;
+    //     const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    //     return `${formattedHours}:${formattedMinutes} ${period}`;
+    // };
 
-    const formatTime = (date) => {
-        const hours = date.getHours();
-        const minutes = date.getMinutes();
-        const period = hours >= 12 ? "PM" : "AM";
-        const formattedHours = hours % 12 || 12;
-        const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-        return `${formattedHours}:${formattedMinutes} ${period}`;
-    };
 
     const user = useContext(UserContext);
+    const [allEvents, setAllEvents] = useState(events);
+    const [therapists, setTherapists] = useState([]);
+    const [services, setServices] = useState([]);
+    const [selectedTherapist, setSelectedTherapist] = useState("");
+    const [newEvent, setNewEvent] = useState({
+        therapist: "",
+        service: "",
+        duration: 60,
+        date: new Date(),
+        time: "12:00 PM",
+        end: new Date(),
+    });    
+
+    useEffect(() => {
+        const timeParts = newEvent.time.split(/:|\s/);
+        const selectedTime = new Date(newEvent.date);
+        selectedTime.setHours(
+            Number(timeParts[0]) + (timeParts[2] === "PM" && timeParts[0] !== "12" ? 12 : 0),
+            Number(timeParts[1]),
+            0
+        );
+        const durationInMinutes = Number(newEvent.duration);
+        const end = addMinutes(selectedTime, durationInMinutes);
+        setNewEvent({ ...newEvent, date: selectedTime, end: end });
+    }, [newEvent.duration, newEvent.time]);
 
 
     useEffect(() => {
         fetchTherapists();
     }, []);
-
     const fetchTherapists = async () => {
         try {
             const response = await fetch(baseUrl + "/all_therapists");
@@ -94,11 +105,9 @@ const SchedulePage = ({ baseUrl }) => {
         }
     };
 
-
     useEffect(() => {
         fetchServices();
     }, [selectedTherapist]);
-
     const fetchServices = async () => {
         if (selectedTherapist) {
             try {
@@ -117,20 +126,22 @@ const SchedulePage = ({ baseUrl }) => {
 
     const handleAddEvent = async (e) => {
         e.preventDefault();
-        setError(null);
-        setLoading(true);
 
         try {
-            const end = new Date(newEvent.start.getTime() + duration * 60000);
             const updatedEvent = {
-                title: `${newEvent.therapist} - ${newEvent.service} - ${duration}`,
-                start: newEvent.start,
-                end: end,
-                user_id: user.user_id // Add the current user's ID to the payload
+                title: `${newEvent.therapist} - ${newEvent.service} - ${newEvent.duration}`,
+                user_id: user.user_id,
+                therapist_id: newEvent.therapist,
+                service: newEvent.service,
+                duration: newEvent.duration,
+                time: newEvent.time,
+                date: newEvent.date,
+                end: newEvent.end,
             };
+            console.log(updatedEvent)
 
             // Make the API request to save the event
-            const response = await fetch(baseUrl + "/create_event", {
+            const response = await fetch(baseUrl + "/create_appointment", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -144,19 +155,16 @@ const SchedulePage = ({ baseUrl }) => {
 
             setAllEvents([...allEvents, updatedEvent]);
             setNewEvent({
-                title: "",
-                start: new Date(),
-                startTime: "",
-                end: new Date(),
                 therapist: "",
-                service: ""
+                service: "",
+                duration: 60,
+                date: new Date(),
+                time: "12:00 PM",
+                end: new Date(),
             });
         } catch (error) {
-            setError("Failed to save event. Please try again.");
             console.error("Error saving event:", error);
         }
-
-        setLoading(false);
     };
 
 
@@ -186,7 +194,9 @@ const SchedulePage = ({ baseUrl }) => {
                         <Form.Select
                             style={{ width: "132px", marginRight: "12px" }}
                             value={newEvent.service}
-                            onChange={(e) => setSelectedService(e.target.value)}
+                            onChange={(e) => {
+                                setNewEvent({ ...newEvent, service: e.target.value })
+                            }}
                         >
                             <option value="">Select a service</option>
                             {services.map((service, index) => (
@@ -200,8 +210,8 @@ const SchedulePage = ({ baseUrl }) => {
                     <label className="form-text">Duration:</label>
                     <Form.Select
                         style={{ width: "90px", marginRight: "10px" }}
-                        value={duration}
-                        onChange={(e) => setDuration(Number(e.target.value))}
+                        value={newEvent.duration}
+                        onChange={(e) => setNewEvent({ ...newEvent, duration: Number(e.target.value) })}
                     >
                         <option value={60}>60 minutes</option>
                         <option value={90}>90 minutes</option>
@@ -214,30 +224,21 @@ const SchedulePage = ({ baseUrl }) => {
                                 placeholderText="Treatment Date"
                                 className="date-picker"
                                 style={{ marginRight: "10px" }}
-                                selected={newEvent.start}
-                                onChange={(start) =>
-                                    setNewEvent({
-                                        ...newEvent,
-                                        start,
-                                        end: new Date(
-                                            start.getTime() + duration * 60000
-                                        ),
-                                    })
-                                }
+                                selected={newEvent.date}
+                                onChange={(date) => setNewEvent({...newEvent, date})}
                             />
                         </div>
                         <div className="time-select-wrapper">
                             <Form.Control
                                 as="select"
                                 className="time-select"
-                                value={formatTime(newEvent.start)}
+                                value={newEvent.time}
                                 onChange={(e) => {
                                     const selectedTime = e.target.value;
-                                    const [hours, minutes, period] = selectedTime.split(/:|\s/);
-                                    const roundedMinutes = minutes >= 30 ? "30" : "00"; // Round minutes to 0 or 30
-                                    const hour = period === "PM" ? parseInt(hours, 10) + 12 : parseInt(hours, 10); // Adjust for PM hours
-                                    const newStart = setHours(setMinutes(newEvent.start, roundedMinutes), hour);
-                                    setNewEvent({ ...newEvent, start: newStart });
+                                    const [hours, minutes] = selectedTime.split(/:|\s/);
+                                    const selectedDate = new Date(newEvent.date);
+                                    selectedDate.setHours(Number(hours), Number(minutes), 0);
+                                    setNewEvent({ ...newEvent, time: selectedTime });
                                 }}
                             >
                                 {Array.from({ length: 8 }, (_, i) => {
