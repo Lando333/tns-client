e0ec0d2cee2e30454550b339ee4801de86d93718
Ryan Rojas <landoslair@gmail.com>
2023-06-26 14:32:58 -0400

    removed log, fixed url

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,19 +1,15 @@
 import React, { createContext, useEffect, useState } from "react";
 import httpClient from "./httpClient";
 
-
-const baseUrl = "http://localhost:5555"
-
 export const UserContext = createContext();
 
-export const UserProvider = ({ children }) => {
+export const UserProvider = ({ children, baseUrl }) => {
     const [user, setUser] = useState(null);
 
     useEffect(() => {
         const fetchUser = async () => {
             try {
                 const resp = await httpClient.get(baseUrl + "/@me");
-                console.log(resp.data)
                 setUser(resp.data);
             } catch (error) {
                 console.log("Not authenticated");
