896e6d0e6f1e1ce1af683b35bd11553c10fe8641
Ryan Rojas <landoslair@gmail.com>
2023-06-26 18:19:49 -0400

    removed text

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -11,20 +11,6 @@ const LandingPage = () => {
         <div className="page-container">
             <img id="header-logo" src={tns_logo} alt="TNS Logo" />
 
-            {user != null ? (
-                <div>
-                    <h2>Logged in</h2>
-                    <h3>ID: {user.user_id}</h3>
-                    <h3>Email: {user.email}</h3>
-
-                </div>
-            ) : (
-                <div>
-                    <p>You are not logged in</p>
-                </div>
-            )}
-
-            <br /> <br /> <br /> 
             <h4>The In-Home Wellness Program You Have Been Looking For</h4>
             <h1>Welcome to Tao Now Solutions!</h1>
             <p>Holistic medicine is the art and science of healing that addresses the whole person – mind, body and spirit. Tao Now Solutions aims to bring that science into your home and every day life. As practitioners of holistic medicine, we integrate conventional and alternative therapies to prevent and treat disease, and most importantly, to promote optimal health. We encourage you to invite us on this wonderful journey of healing.</p>
