44eb8a417fe4b85970398b04836b27472c2a3c2f
Ryan Rojas <landoslair@gmail.com>
2023-06-26 18:31:34 -0400

    added TherapistPage

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,6 +3,32 @@ import { Link } from "react-router-dom";
 import LoginBox from "./LoginBox";
 
 const NavBar = ({ baseUrl }) => {
+    const user = useContext(UserContext);
+    const [isTherapist, setIsTherapist] = useState(false);
+
+    useEffect(() => {
+        const fetchTherapists = async () => {
+            try {
+                const response = await fetch(`${baseUrl}/all_therapists`);
+                if (response.ok) {
+                    const therapists = await response.json();
+                    const therapistIds = therapists.map((therapist) => therapist.therapist_id);
+                    setIsTherapist(therapistIds.includes(user.therapist_id));
+                } else {
+                    // Handle error response
+                    console.error("Failed to fetch therapists");
+                }
+            } catch (error) {
+                // Handle fetch error
+                console.error("Error while fetching therapists", error);
+            }
+        };
+
+        if (user && user.role === "therapist") {
+            fetchTherapists();
+        }
+    }, [baseUrl, user]);
+
     return (<>
         <nav>
             <ul className="navbar">
@@ -18,6 +44,9 @@ const NavBar = ({ baseUrl }) => {
                 <li>
                     <Link to="/about">About</Link>
                 </li>
+                {isTherapist && <li>
+                    <Link to="/therapist">Therapist Profile</Link>
+                </li>}
             </ul>
             <LoginBox baseUrl={baseUrl} />
         </nav>
---
+++ src/pages/TherapistPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TherapistPage = () => {
+
+    return (
+        <div>
+            <h1>Coming Soon!</h1>
+        </div>
+    );
+};
+
+export default TherapistPage;
