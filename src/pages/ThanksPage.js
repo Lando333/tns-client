1dbea5dc148725b2bb4370110a29ef0326981c75
Ryan Rojas <landoslair@gmail.com>
2023-06-28 14:11:18 -0400

    cleaned up ThanksPage

--- src/pages/ThanksPage.js
+++ src/pages/ThanksPage.js
@@ -1,20 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React from 'react';
 
 function ThanksPage() {
-    const location = useLocation();
-    const [sessionID, setSessionID] = useState('');
-
-    useEffect(() => {
-        const searchParams = new URLSearchParams(location.search);
-        const sessionID = searchParams.get('session_id');
-        setSessionID(sessionID);
-    }, [location.search]);
 
     return (
         <div>
-            <h1>Thank you for booking with us!</h1>
-            {sessionID && <p>Session ID: {sessionID}</p>}
+            <h2>Thank you for booking with us!</h2>
         </div>
     );
 }
